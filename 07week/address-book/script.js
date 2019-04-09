7e660235883026b530133df5da53d95a8c9fe58e
Emilio Sanchez <sanchez.emilio483@gmail.com>
2019-04-09 15:45:11 -0500

    allow buttons to remove displayed data

--- 07week/address-book/script.js
+++ 07week/address-book/script.js
@@ -7,20 +7,15 @@ function getUsers(){
   fetch('https://randomuser.me/api/?results=5')
   .then((res)=>res.json())
   .then((data)=> {
-    // console.log(data)
     let arr = []
     data.results.forEach(item => {
-      // console.log(item)
-      arr.push(item)
-      // .name.first + ' ' + item.name.last
-      
+      arr.push(item)      
     });
-    console.log(arr)
     let count = 0
     users = arr
     arr.forEach(user =>{
       let somethin = user
-      document.getElementById('output').innerHTML += `<img src=${user.picture.medium}></img>  ` +user.name.first +' '+ user.name.last + ' ' + `<button onClick = "display(${count})">click</button> <div id = 'space${count}'> </div><br>`
+      document.getElementById('output').innerHTML += `<img src=${user.picture.medium}></img>  ` +user.name.first +' '+ user.name.last + ' ' + `<button onClick = "display(${count})">click</button> <div id = 'space${count}'></div><br>`
       count++;
     })
   }).catch((err) => console.log(err))
@@ -28,5 +23,12 @@ function getUsers(){
 getUsers()
 
 function display(num){
-  document.getElementById(`space${num}`).innerHTML = `Age: ` + users[num].dob.age + ` <br>  Address: ` + users[num].location.street + `, ` + users[num].location.city + `, ` + users[num].location.state
+  if(document.getElementById(`space${num}`).innerHTML === ''){
+    document.getElementById(`space${num}`).innerHTML = `Age: ` + users[num].dob.age + ` <br>  Address: ` + users[num].location.street + `, ` + users[num].location.city + `, ` + users[num].location.state
+  }
+  else{
+    document.getElementById(`space${num}`).innerHTML = ''
+
+  }
+
 }
